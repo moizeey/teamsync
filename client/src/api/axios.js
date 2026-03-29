d082ba051a2541b2ad8ebdb2a437cacdf9976b7e
Moizz <moizeey17@gmail.com>
2026-03-29 22:49:07 +0500

    Update frontend API URL to new Vercel backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:5000/api',
-    baseURL: 'https://teamsync-backend-five.vercel.app/api'
+    baseURL: 'https://teamsync-server-xi.vercel.app/api'
 });
 
 // Add a request interceptor
